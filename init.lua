5ecde837530b2a021a9a81dbe90b2fb9befe6898
Ali <alialialiawsd666@gmail.com>
2026-04-10 08:04:26 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,188 @@
+vim.opt.number = true                -- Show line numbers
+vim.opt.relativenumber = true                -- Show line numbers
+vim.opt.tabstop = 4                  -- Tab = 4 spaces
+vim.opt.softtabstop = 4              -- Backspace removes 4 spaces
+vim.opt.shiftwidth = 4               -- Indent = 4 spaces
+vim.opt.expandtab = true             -- Use spaces instead of tabs
+vim.opt.hlsearch = false
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.o.eventignore = "FocusGained,FocusLost"
+--vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/site")
+
+-- Bootstrap lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+
+            -- REQUIRED
+            harpoon:setup()
+            -- REQUIRED
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+            vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+            vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
+            vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+            vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        end,
+    },
+    {
+
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            -- NOTE: 'nvim-treesitter.configs' is gone. 
+            -- Use the main module instead.
+            require("nvim-treesitter").setup({
+
+                -- ensure_installed is still the best way to prevent the download loop
+                ensure_installed = { 
+                    "c", "cpp", "lua", "vim", "vimdoc", "query", 
+                    "markdown", "markdown_inline", "python" 
+                },
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+        config = function()
+            local highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
+            local hooks = require "ibl.hooks"
+            -- create the highlight groups in the highlight setup hook, so they are reset
+            -- every time the colorscheme changes
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+
+            vim.g.rainbow_delimiters = { highlight = highlight }
+            require("ibl").setup { scope = { highlight = highlight } }
+
+            hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)    end,
+        },
+        {
+            "windwp/nvim-autopairs",
+            event = "InsertEnter",
+            config = function()
+                require("nvim-autopairs").setup({
+                    check_ts = true, -- Enable Treesitter integration
+                })
+            end
+        },
+        {
+            'nvim-lualine/lualine.nvim',
+            dependencies = { 'nvim-tree/nvim-web-devicons' },
+            config = function()
+                require('lualine').setup()
+            end,
+        },
+        {
+            "nvim-neo-tree/neo-tree.nvim",
+            branch = "v3.x",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "MunifTanjim/nui.nvim",
+                "nvim-tree/nvim-web-devicons", -- optional, but recommended
+            },
+            lazy = false, -- neo-tree will lazily load itself
+        }
+
+    }
+
+
+    local opts = {}
+
+
+    require("lazy").setup(plugins, opts)
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set('n', '<leader>ff',builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg',builtin.live_grep, {})
+
+    --vim.keymap.set('n', '<leader>n',':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
+    vim.keymap.set('i','jj','<ESC>')
+    vim.keymap.set('n','<leader>w',':w<CR>')
+    --vim.keymap.set('i', '<Esc>', '~', { noremap = true })
+    vim.keymap.set('i', '<C-h>','<Left>')
+    vim.keymap.set('i', '<C-j>','<Down>')
+    vim.keymap.set('i', '<C-k>','<Up>')
+    vim.keymap.set('i', '<C-l>','<Right>')
+
+
+    require("catppuccin").setup({
+        integrations = {
+        treesitter = true,
+        native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+            },
+        },
+        indent_blankline = {
+            enabled = true,
+            scope_color = "lavender", -- Makes your bracket lines look better
+        },
+    },
+})
+    vim.cmd.colorscheme "catppuccin"
+
