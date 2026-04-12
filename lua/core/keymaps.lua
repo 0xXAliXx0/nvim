a404db4be98404df34785a5e048adad73f9a0615
0xXAliXx0 <alialialiawsd666@gmail.com>
2026-04-12 07:58:58 +0300

    added more plugins

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,10 @@
+
+            --vim.keymap.set('n', '<leader>n',':Neotree filesystem reveal left<CR>', {})
+            vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
+            vim.keymap.set('i','jj','<ESC>')
+            vim.keymap.set('n','<leader>w',':w<CR>')
+            --vim.keymap.set('i', '<Esc>', '~', { noremap = true })
+            vim.keymap.set('i', '<C-h>','<Left>')
+            vim.keymap.set('i', '<C-j>','<Down>')
+            vim.keymap.set('i', '<C-k>','<Up>')
+            vim.keymap.set('i', '<C-l>','<Right>')
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,15 +7,6 @@ return{
             vim.keymap.set('n', '<leader>ff',builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg',builtin.live_grep, {})
 
-            --vim.keymap.set('n', '<leader>n',':Neotree filesystem reveal left<CR>', {})
-            vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
-            vim.keymap.set('i','jj','<ESC>')
-            vim.keymap.set('n','<leader>w',':w<CR>')
-            --vim.keymap.set('i', '<Esc>', '~', { noremap = true })
-            vim.keymap.set('i', '<C-h>','<Left>')
-            vim.keymap.set('i', '<C-j>','<Down>')
-            vim.keymap.set('i', '<C-k>','<Up>')
-            vim.keymap.set('i', '<C-l>','<Right>')
 
         end,
 
